26b4fc6b32c04bb3256d2eddd806b2f2dff929a3
darnacodes <nadia.adarna@gmail.com>
2022-12-03 21:29:46 +0800

    finished level 1.3

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.json())
+
 
 const welcomeMessage = {
   id: 0,
@@ -14,12 +18,63 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+//create new message 
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000")
+app.post("/messages", function (request,response){
+const newMessage= request.body
+newMessage.id=messages.length
+console.log(newMessage)
+//validation reject request that has an empty or missing text 
+if (notValidRequest(message)){
+  response.status(400).send({sucess:false})
+}
+
+messages.push(newMessage)
+response.status(201).send({success:true})
+});
+
+function notValidRequest(message){
+  return  message.id == undefined ||
+     message.from == undefined ||
+     message.text == undefined 
+     
+  }
+
+//read all messages
+app.get("/messages", function (request, response) {
+  response.send(messages);
+});
+
+
+//read one message by id
+app.get("/messages/:id", function (request, response){
+
+  const id= request.params.id
+  console.log(id)
+
+  const searchMessageId= messages.find(message=>message.id==id)  
+  console.log(searchMessageId)
+  response.send(searchMessageId)
+
+});
+
+//delete message by id
+app.delete("/messages/:id", function (request, response){
+const id=request.params.id
+const messageFiltered= messages.filter(function(message){
+  return message.id !=id})
+messages=messageFiltered
+response.send({sucess:true})
+
+});
+
+//port
+const port= 4000
+app.listen(port, () => {
+   console.log("Listening on port" + port )
   });
